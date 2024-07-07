33d51d345f471e071fdde39d50411ca1517b2801
Fraser <fraser.brooksbaillie@snowedin.ca>
2024-07-06 20:31:34 -0400

    Test to see

--- GameEngine/States/Editor/CursorState.h
+++ GameEngine/States/Editor/CursorState.h
@@ -84,7 +84,7 @@ protected:
     const Vec3f c_SelectedBoxColour = Vec3f(0.f / 255.f, 255.f / 255.f, 255.f / 255.f);
     const Vec3f c_InspectorColour = Vec3f(175.f / 255.f, 225.f / 255.f, 175.f / 255.f);
 };
-
+//
 class SelectedModel : public ISelectedObject
 {
 public:
